6e22d1f94a76b911157f395744c166283ed5673f
Nurlan <iamsamurai1991@gmail.com>
2024-06-23 23:57:05 +0200

    new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   src/App.jsx 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   src/redux-app/features/counter/counterSlice.js 	new file:   src/redux-app/store.js 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useSelector, useDispatch } from "react-redux";
+import { FaMinus } from "react-icons/fa6";
+import { FaPlus } from "react-icons/fa";
+import { Button } from "@mui/material";
+import {
+  increase,
+  decrease,
+  handleMode,
+} from "./redux-app/features/counter/counterSlice";
+const App = () => {
+  const count = useSelector((state) => state.count.value);
+  const mode = useSelector((state) => state.count.mode);
+  const dispatch = useDispatch();
+  return (
+    <div className={`wrapper ${!mode && "dark"}`}>
+      <div className="container">
+        <Button variant="contained" color="success">
+          <FaPlus />
+        </Button>
+        <Button variant="outlined" color="error">
+          <FaMinus />
+        </Button>
+        <h1>{count}</h1>
+
+        <Button onClick={() => dispatch(handleMode())} variant="contained">
+          Mode
+        </Button>
+      </div>
+    </div>
+  );
+};
+export default App;
---
+++ src/redux-app/features/counter/counterSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: { value: 0, mode: false },
+  reducers: {
+    increase: (state) => {
+      state.value += 1;
+    },
+    decrease: (state) => {
+      state.value -= 1;
+    },
+    handleMode: (state) => {
+      state.mode = !state.mode;
+    },
+  },
+});
+export const { increase, decrease, handleMode } = counterSlice.actions;
+export default counterSlice.reducer;
